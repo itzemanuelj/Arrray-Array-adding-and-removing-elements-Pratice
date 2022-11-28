a9dd949c9c75e6a362c34ec9d729c8dba0192c87
Emanuel Joness <itzemanuelj@gmail.com>
2022-11-27 21:03:18 -0500

    Create array.js

---
+++ array.js
@@ -0,0 +1,45 @@
+///What is a variable that represents a list of values? Answer with one word.
+///array
+
+///An array comprises any number of ______. ("Values" would be correct, but I'm looking for another word.)
+///element
+
+///The number inside the square brackets is the element's ____.
+///index
+
+///The number inside the square brackets is the element's ____.
+0
+
+let numberIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+///Create a new array, nums, comprising three numbers from 3 to 5.
+let nums = [3, 4, 5]
+
+///This array has been defined.
+
+var names = ["Alf", "Bo", "Gail"];
+
+///Specifying an array element, code an alert that displays "Gail".
+
+alert(names[2]);
+
+///Create an array with a single element, a string. The array hasn't been declared beforehand
+let singleElement = ['a string'];
+
+///Create an array with three elements that are numbers. It hasn't been declared beforehand.
+threeElementArray = [1, 2, 3];
+
+///Assign to a variable the first element of an array. The variable hasn't been declared beforehand.
+let airlines = airlines[0];
+
+/// Create an array with three elements: a number, a string, and a variable. The array has been declared beforehand.
+randomArray = [1, 'one' , one];
+
+/// Create an array with two string elements. Then assign the last element in the array to a variable. Neither the array nor the variable have been declared beforehand.
+let airlinesArray = ['detla', 'AA'];
+let favAirlines = airlinesArray[1];
+
+/// This array has been defined.
+var names = ["Alf", "Bo", "Gail"];
+///Specifying an array element, code an alert that displays "Gail".
+
