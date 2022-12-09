ba7ccdc52e1951d7ab2a92fe37f91e46805cba25
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-08 21:30:20 -0500

    Update add remove array.js

--- add remove array.js
+++ add remove array.js
@@ -7,7 +7,7 @@ counties[0];
 
 /// The pop() method removes the last element from an array and returns that element. This method changes the length of the array.
 /// The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end 
-///The push() method adds one or more elements to the end of an array and returns the new length of the array.
+/// The push() method adds one or more elements to the end of an array and returns the new length of the array.
 
 /// What keyword removes the last element from an array?
 array.pop();
@@ -15,7 +15,7 @@ array.pop();
 ///What keyword adds one or more elements to the end of an array?
 array.push();
 
-// Assign the number 1 to the 10th element of the array nums.
+/// Assign the number 1 to the 10th element of the array nums.
 nums[9] = 1;
 
 ///Remove the last element from the array pets.
@@ -28,12 +28,14 @@ emptyArray = [];
 let newArray = [1,2,3,4];
 newArray[0] = 'string';
 
-// Add a new element, a number, to the end of an array.
+/// Add a new element, a number, to the end of an array.
 newArray.push(99);
 
 ///Assign string values to the second and fourth elements of an array.
 array[1] = 'string';
 array[3] = 'number';
 
-// Add three number elements to end of an array.
+/// Add three number elements to end of an array.
 array.push(99, 11, 26);
+
+
